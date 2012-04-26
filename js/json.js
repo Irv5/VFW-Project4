b7e9c41b406c77ce38e3ac574497cc965252025a
Irvin Reaves <irv5@Irvins-MacBook-Pro.local>
2012-04-25 21:46:50 -0400

    Updated the main.js file and created the json.js file with week 4 assignment.

---
+++ js/json.js
@@ -0,0 +1,59 @@
+// Irvin Reaves
+// Project 4
+// Visual Frameworks (VFW) 1204
+// Mobile Development
+// Full Sail University
+// Music Catalog js
+
+
+var json = {
+	"song1": {
+		"songInfo": ["Select Song Type:", "Party"],
+		"sname": ["Name of Song:", "Raw"],
+		"aname": ["Album:", "Raw"],
+		"arname": ["Artist:", "Kane"],
+		"url": ["Music Down website:", "http://www.apple.com"],
+		"favorite": ["Favorite:", "Yes"],
+		"fav": ["Save as Favorite:", "Yes"],
+		"range": ["Rating:", "5"],
+		"date": ["Date:", "2012-4-21"],
+		"notes": ["Notes:", "VFW"]
+	},
+	"song2": {
+		"songInfo": ["Select Song Type:", "Relaxed"],
+		"sname": ["Name of Song:", "Live"],
+		"aname": ["Album:", "Live"],
+		"arname": ["Artist:", "Live Band"],
+		"url": ["Music Down website:", "http://www.yahoo.com"],
+		"favorite": ["Favorite:", "No"],
+		"fav": ["Save as Favorite:", "Yes"],
+		"range": ["Rating:", "7"],
+		"date": ["Date:", "2012-4-19"],
+		"notes": ["Notes:", "VFW"]
+	},
+	"song3": {
+		"songInfo": ["Select Song Type:", "Fun"],
+		"sname": ["Name of Song:", "Good Times"],
+		"aname": ["Album:", "Funk Band"],
+		"arname": ["Artist:", "Tim"],
+		"url": ["Music Down website:", "http://www.rhaposody.com"],
+		"favorite": ["Favorite:", "Yes"],
+		"fav": ["Save as Favorite:", "Yes"],
+		"range": ["Rating:", "4"],
+		"date": ["Date:", "2012-4-22"],
+		"notes": ["Notes:", "VFW"]
+	},
+	"song4": {
+		"songInfo": ["Select Song Type:", "Other"],
+		"sname": ["Name of Song:", "Jazz Cool"],
+		"aname": ["Album:", "Jazz Time"],
+		"arname": ["Artist:", "Cool Breeze"],
+		"url": ["Music Down website:", "http://www.apple.com"],
+		"favorite": ["Favorite:", "Yes"],
+		"fav": ["Save as Favorite:", "Yes"],
+		"range": ["Rating:", "10"],
+		"date": ["Date:", "2012-4-17"],
+		"notes": ["Notes:", "VFW"]
+	}
+
+}
